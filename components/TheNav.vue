e8d9f6fb6d85adfdd79ea5afacc1b9f8725f8340
Jaime Aleman <jjaimealeman@gmail.com>
2022-03-24 22:02:46 -0600

    ++ /components.

---
+++ components/TheNav.vue
@@ -0,0 +1,15 @@
+<template>
+    <NuxtLink to="https://nuxtjs.org" target="_blank">Nuxt website</NuxtLink>
+</template>
+
+
+<script>
+
+</script>
+
+
+<style>
+body {
+    color: #333;
+}
+</style>
