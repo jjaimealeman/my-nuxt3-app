676c17db9a2804143a0ed1a1428eabf5475f9f12
Jaime Aleman <jjaimealeman@gmail.com>
2022-11-02 01:02:35 -0600

    Feat: add page transitions.

--- app.vue
+++ app.vue
@@ -41,4 +41,11 @@
   p {
       @apply font-sans leading-9;
   }
+  .page-enter-active, .page-leave-active {
+    transition: all 0.4s;
+  }
+  .page-enter-from, .page-leave-to {
+    opacity: 0;
+    filter: blur(1rem)
+  }
 </style>
