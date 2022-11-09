73be31616484fc7552c20ad36e8a03276cdeb844
Jaime Aleman <jjaimealeman@gmail.com>
2022-11-09 12:13:52 -0700

    ADD vite-plugin-vue-inspector.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,20 @@
-import { defineNuxtConfig } from 'nuxt'
-
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
+import Inspector from "vite-plugin-vue-inspector"
+
 export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
   build: {
     postcss: {
       postcssOptions: require('./postcss.config.js'),
     },
+  },
+  vite: {
+    plugins: [
+      Inspector({
+        appendTo: "entry.mjs",
+        enabled: true,
+        toggleButtonVisibility: "always",
+      })
+    ]
   }
 })
