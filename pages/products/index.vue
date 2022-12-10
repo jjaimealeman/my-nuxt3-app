13e98c441745d2a4cace301a35eb36466b8bb02a
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-10 11:55:41 -0700

    Add product meta data.

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,11 @@
 <template>
     <div>
+        <Head>
+            <Title>{{ route.meta.title }} </Title>
+          </Head>
+
         <h2>Products</h2>
+
         <div class="grid grid-cols-4 gap-5">
             <div v-for="p in products">
                 <ProductCard :product="p" />
@@ -9,14 +14,29 @@
     </div>
 </template>
 
+<script>
+export default {
+  head: {
+    meta: [
+      {
+        hid: "description",
+        name: "description",
+        content: "A Nuxt3 app ... with Products.",
+      },
+    ],
+  },
+};
+</script>
 
 <script setup>
 definePageMeta({
-    title: "Products Page"
-    // layout: 'products'
+    title: "Products Page",
+    layout: 'products'
 });
 const { data: products } = await useFetch('https://fakestoreapi.com/products')
 
+const route = useRoute();
+console.log(route.meta.title);
 </script>
 
 
