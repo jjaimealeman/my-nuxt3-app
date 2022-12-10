06482c09a3cec41ebf0b18b9d5e42cda921f7f8b
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-10 13:08:12 -0700

    Add product meta date: title and description.

--- pages/products/[id].vue
+++ pages/products/[id].vue
@@ -1,5 +1,9 @@
 <template>
     <div>
+        <Head>
+            <Title>test | {{ product.title }}</Title>
+            <Meta name="description" :content="product.description" />
+        </Head>
         <ProductDetails :product="product" />
     </div>
 </template>
