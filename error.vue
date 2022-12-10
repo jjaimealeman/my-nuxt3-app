fb70d9c79bdddaef105ee3ed0e4938eb07760235
Jaime Aleman <jjaimealeman@gmail.com>
2022-12-09 18:23:57 -0700

    Add Error page.

---
+++ error.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="max-w-sm mx-auto text-center mt-7 card">
+        <p class="text-4xl font-bold mt-7">{{ error.statusCode }}</p>
+        <p class="mt-7">OOOPSS!!!</p>
+        <p class="mt-7">{{ error.message }}</p>
+        <button class="btn my-7" @click="handleClearError">Go Home ... </button>
+    </div>
+</template>
+
+
+<script setup>
+    defineProps(['error'])
+
+    const handleClearError = () => clearError({ redirect: '/' })
+</script>
+
+
+<style scoped>
+
+</style>
